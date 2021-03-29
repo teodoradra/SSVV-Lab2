845c989b736d498d77a5ad1165c1b6aea608fe7c
Teo <dtie2524@scs.ubbcluj.ro>
2021-03-29 10:28:42 +0300

    Lab 3 TH

--- src/test/java/ssv/lab2/TestAddAssignment.java
+++ src/test/java/ssv/lab2/TestAddAssignment.java
@@ -52,13 +52,83 @@ public class TestAddAssignment {
 
     @Test
     public void testAddAssignmentSuccess() {
-
         assertEquals(service.saveTema("1", "First Lab", 8, 6), 1);
     }
 
     @Test
     public void testAddAssignmentFailure() {
         assertEquals(service.saveTema("7", null, 8, 7), 0);
+    }
+
+    @Test
+    public void TC1_WBT_AddAssignment() {
+        assertEquals(service.saveTema("", "hw", 5, 3), 0);
+    }
+
+    @Test
+    public void TC2_WBT_AddAssignment(){
+        assertEquals(service.saveTema("0", "", 5, 3), 0);
+    }
+
+    @Test
+    public void TC3_WBT_AddAssignment(){
+        assertEquals(service.saveTema("1", "hw", 1, 9), 0);
+    }
+
+    @Test
+    public void TC4_WBT_AddAssignment(){
+        assertEquals(service.saveTema("2", "hw", 4, 0), 0);
+    }
+
+    @Test
+    public void TC5_WBT_AddAssignment(){
+        assertEquals(service.saveTema("0", "hw", 5, 3), 1);
+        assertEquals(service.saveTema("0", "hw", 5, 3), 0);
+    }
+
+    @Test
+    public void TC6_WBT_AddAssignment(){
+        assertEquals(service.saveTema("3", "hw", 5, 3), 1);
+    }
+
+    @Test
+    public void TC7_WBT_AddAssignment(){
+        assertEquals(service.saveTema("4", "hw", 5, 3), 1);
+    }
+
+    @Test
+    public void TC8_WBT_AddAssignment(){
+        assertEquals(service.saveTema("5", "hw", 5, 3), 1);
+    }
+
+    @Test
+    public void TC9_WBT_AddAssignment(){
+        assertEquals(service.saveTema("6", "hw", 5, 3), 1);
+    }
+
+    @Test
+    public void TC10_WBT_AddAssignment() {
+        assertEquals(0, service.saveTema(null, "hw", 5, 3));
+    }
+
+    @Test
+    public void TC11_WBT_AddAssignment() {
+        assertEquals(0, service.saveTema("7", null, 5, 3));
+    }
+
+    @Test
+    public void TC12_WBT_AddAssignment() {
+        assertEquals(0, service.saveTema("8", "hw", 21, 9));
+    }
+
+    @Test
+    public void TC13_WBT_AddAssignment() {
+        assertEquals(0, service.saveTema("9", "hw", 0, 0));
+    }
+
+    @Test
+    public void TC14_WBT_AddAssignment() {
+        assertEquals(0, service.saveTema("10", "hw", 9, 21));
     }
 }
 
